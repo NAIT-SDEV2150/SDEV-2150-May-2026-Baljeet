5b8c0e7b2ec69681a5f8ad6f66b7fa280536bdfa
BaljeetK79 <baljeetk@nait.ca>
2026-06-05 19:18:34 -0400

    updated prop example

--- Simple React examples to explain concepts/ExplainProps/src/App.jsx
+++ Simple React examples to explain concepts/ExplainProps/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <h1> This is top level component: APP</h1>
-      <User name ="Alexa" age={25}/>
+      <User username ="Alexa" age={25} emailid ="abc@gmail.com"/>
 
     </>
   )
--- Simple React examples to explain concepts/ExplainProps/src/User.jsx
+++ Simple React examples to explain concepts/ExplainProps/src/User.jsx
@@ -3,8 +3,10 @@ function User(prop) {
 
   return (
     <>
-      <h1> USERNAME: {prop.name}</h1>
+      <h1> USERNAME: {prop.username}</h1>
       <h1> AGE: {prop.age}</h1>
+      <h1> EMAIL: {prop.emailid}</h1>
+      
     </>
   )
 }
