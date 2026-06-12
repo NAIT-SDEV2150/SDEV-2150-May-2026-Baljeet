71de7dd0a0b102db65c22f268892739e866e8426
BaljeetK79 <baljeetk@nait.ca>
2026-06-12 19:12:41 -0400

    Lesson 09 Presentational/Container updated

---
+++ 05-component-composition-styling/day-09/lesson-09-classWalkthrough/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './components/Header';
+import Filters from './components/Filters';
+import Results from './components/Results';
+import Details from './components/Details';
+import PageLayout from './components/layout/PageLayout';
+
+function App() {
+  return (
+    <PageLayout 
+    header = {<Header  tagline="Find the right resources, right away" /> }
+    left= {<Filters/> }
+    middle = {<Results/>} 
+    right = {<Details/>}
+    ></PageLayout>
+  );
+}
+
+export default App;
