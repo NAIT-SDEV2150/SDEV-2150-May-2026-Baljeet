5d03167adfd26b400f4161a235039490bbd5487f
BaljeetK79 <baljeetk@nait.ca>
2026-06-23 18:51:34 -0400

    day 13 updated

---
+++ 07-lifting-state-communication/day-13/lesson-13-completed/src/App.jsx
@@ -0,0 +1,62 @@
+import Header from './components/Header';
+import Filters from './components/Filters';
+import Results from './components/Results';
+import Details from './components/Details';
+import PageLayout from './components/layout/PageLayout';
+import { useState } from 'react';
+
+function App() {
+
+  // App.jsx
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [openNowOnly, setOpenNowOnly] = useState(false);
+  const [virtualOnly, setVirtualOnly]= useState(false);
+  const [selectedResource, setSelectedResource] = useState(null);
+
+
+  return (
+    <PageLayout header={<Header tagline="Find the right resources, right away" />}>
+      <aside className="md:col-span-3 lg:col-span-1">
+        <Filters 
+          searchTerm={searchTerm}
+          onSearchChange={setSearchTerm}
+          selectedCategories={selectedCategories}
+          onCategoryToggle={setSelectedCategories}
+          openNowOnly={openNowOnly}
+          onOpenNowChange={setOpenNowOnly}
+          virtualOnly={virtualOnly}
+          onVirtualOnlyChange={setVirtualOnly}
+          />
+      </aside>
+      <section className="md:col-span-2 lg:col-span-1">
+        <Results           
+          selectedResource={selectedResource}
+          onSelectResource={setSelectedResource}
+          searchTerm={searchTerm}
+          selectedCategories={selectedCategories}
+          openNowOnly={openNowOnly}
+          virtualOnly={virtualOnly}
+           />
+      </section>
+      <aside className="md:col-span-1 lg:col-span-1">
+        {selectedResource? (
+        <Details resource={selectedResource}  />
+        ):
+        (<div className="text-sm text-base-content/70">
+            Select a resource to view details.
+          </div>)}
+      </aside>
+    </PageLayout>
+    // <PageLayout
+    //   header={<Header tagline="Find the right resources, right away" />}
+    //   left={<Filters />}
+    //   middle={<Results />}
+    //   right={<Details />}
+    // />
+
+  );
+}
+
+export default App;
